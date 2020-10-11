8ade756d1f2a9f5e27e7b4473cf4af955d34382b
Jeffrey Zhao <jeffreyzhao2003@gmail.com>
2020-10-11 17:42:19 -0400

    Update
    Updated account and market's bugs with the database and discord.py

--- cogs/accounts.py
+++ cogs/accounts.py
@@ -1,31 +1,34 @@
-
+import traceback
 from datetime import timedelta
 import discord
 from discord.ext import commands
-import datetime
-from databridge import * 
+from datetime import datetime
+
+import sys
+sys.path.append(sys.path[0] + "/..")
+from databridge import *
 
 userbridge = userBridge()
 itembridge = itemBridge()
 
 
 class Accounts(commands.Cog):
-    
+
     @commands.command(aliases = ['reg'])
     #register
     async def register(self, ctx):
         user = ctx.message.author
         try:
             dbUser = userbridge.Get(str(user))
-            if(dbUser):
+            if(dbUser != None):
                 await ctx.send(f"{user.mention} is already registered")
             else:
-                userDict = {'username': str(user), 'balance': 1000, 'dropdate' : None}
+                userDict = {'username': str(user), 'balance': 1000, 'dropDate' : None}
                 userbridge.Add(userDict)
                 await ctx.send(f"{user.mention} is now registered")
         except:
             await ctx.send(f"There was an error, try again later")
-
+            traceback.print_exc()
     
     @commands.command(aliases = ['bal'])
     #balance of either the user or someone else
@@ -42,61 +45,62 @@ class Accounts(commands.Cog):
                     await ctx.send(f"{user.mention} has not been registered yet. Use $register")
             except:
                 await ctx.send(f"There was an error, try again later")
+                traceback.print_exc()
         #check bal of other user
         else:
             try:
-                dbMember = userbridge.Get(member)
+                dbMember = userbridge.Get(str(member))
                 if(dbMember):
                     await ctx.send(f"{member} has {dbMember.balance} credit(s)")
                 else:
-                    await ctx.send(f"{member} has not been registered yet. Use $register")
+                    await ctx.send(f"{member} has not been registered yet. They should use $register")
             except:
                 await ctx.send(f"There was an error, try again later")
+                traceback.print_exc()
     
     #Tranfer money
     @commands.command()
-    async def transfer(self, ctx, amount: int, reciever: discord.Member=None):
+    async def transfer(self, ctx, amount: int, reciever: discord.Member):
         user = ctx.message.author
-        if (reciever):
-            try:
-                dbUser = userbridge.Get(str(user)).to_dict()
-                dbReciever = userbridge.Get(str(reciever)).to_dict()
-                if (amount <= 0):
-                    await ctx.send('You can only tranfer an positive integer amount of credits')
-                if (amount > 0):
-                    if(amount > dbUser['balance']):
-                        await ctx.send(f'Cannot transfer {amount} credit(s) since your account does not have that much')
-                    elif(dbReciever == None):
-                        await ctx.send(f'{reciever} has not been registered so you cannot send the user credits')
+        try:
+            dbUser = (userbridge.Get(str(user))).to_dict()
+            dbReciever = (userbridge.Get(str(reciever))).to_dict()
+            if (amount < 0):
+                await ctx.send('You can only tranfer an positive integer amount of credits')
+            else:
+                if(amount > dbUser['balance']):
+                    await ctx.send(f'Cannot transfer {amount} credit(s) since your account does not have that much')
                 else:
                     dbUser['balance'] -= amount
                     dbReciever['balance'] += amount
                     userbridge.Update(dbUser)
                     userbridge.Update(dbReciever)
                     await ctx.send(f"{amount} credit(s) has been successfully tranfered")
-            except:
-                await ctx.send(f"There was an error, try again later")
-        else:
-            await ctx.send('You need to specfify someone to transer credits to')
-
+        except AttributeError:
+            await ctx.send(f'{reciever} has not been registered so you cannot send the user credits')
+            traceback.print_exc()
+        except:
+            await ctx.send(f"There was an error, try again later")
+            traceback.print_exc()
     #daily drops
     @commands.command()
     async def daily(self, ctx):
         user = ctx.message.author
         try:
             dbUser = userbridge.Get(str(user)).to_dict()
+            dropdate = userbridge.Get(str(user)).last_dropdate
             #if this is the first time the user has used the daily command
-            if(dbUser['dropdate'] == None):
-                dbUser['dropdate'] = datetime.now()
+            if(dropdate == None):
+                dbUser['dropDate'] = (datetime.now()).strftime('%Y-%m-%d %H:%M:%S')
                 dbUser['balance'] += 200000
                 userbridge.Update(dbUser)
                 await ctx.send('Since this your first time recieving the daily drop, here is a 20,000 drop. Come back in 24 hours to recieve another drop')
             else:
                 #must be more than one day that elasped
                 #time diff is the time that has currently elasped
-                timeDiff = datetime.now() - dbUser['dropdate']
+                timeDiff = datetime.now() - datetime.strptime(str(dropdate), '%Y-%m-%d %H:%M:%S') 
                 if(timeDiff >= timedelta(days=1)):
-                    dbUser['dropdate'] = datetime.now()
+                    dbUser['dropDate'] = (datetime.now()).strftime('%Y-%m-%d %H:%M:%S')
                     await ctx.send(f"Here is your daily drop of 500 credits {user.mention}")
                     dbUser['balance'] += 500
                 else:
@@ -107,5 +111,6 @@ class Accounts(commands.Cog):
                     await ctx.send(f"Sorry, your daily drop is not yet ready. Come back in {hours} hour(s) and {mins} minute(s)")
         except:
             await ctx.send(f"There was an error, try again later")
+            traceback.print_exc()
 def setup(bot):
     bot.add_cog(Accounts(bot))    
--- cogs/market.py
+++ cogs/market.py
@@ -3,10 +3,11 @@ from discord.ext import commands
 from datetime import datetime, timedelta
 from random import random, randint, uniform
 from time import sleep
-import os
+import traceback
+import sys
+sys.path.append(sys.path[0] + "/..")
 from databridge import *
 
-images = os.listdir(os.path.join(os.getcwd(),'images'))
 userbridge = userBridge()
 itembridge = itemBridge()
 
@@ -22,20 +23,21 @@ class Market(commands.Cog):
         item = {'name': name, 'quantity' : quantity, 'price': price, 'owner': str(user)}
         try:
             itembridge.Add(item)
-            await ctx.send(f'Your item {name} is now on the market')
+            await ctx.send(f'Your item: {name} is now on the market')
         except:
-            await ctx.send(f'Your item {name} could not be added')
-
+            await ctx.send(f'Your item: {name} could not be added')
+            traceback.print_exc()
         
     @commands.command()
     async def buy(self, ctx, id: int, quantity: int):
         user = ctx.message.author
         try:
-            dbUser = userbridge.Get(user).to_dict()
+            dbUser = userbridge.Get(str(user)).to_dict()
             dbItem = itembridge.GetByID(id).to_dict()
             dbReciever = userbridge.Get(dbItem['owner']).to_dict()
             if(dbItem['quantity'] < quantity):
                 await ctx.send(f'There are/is only {dbItem["quantity"]} avaliable')
+            else:
                 if(dbUser['balance'] < dbItem['price']*quantity):
                     await ctx.send(f"You don't have enough credits to buy. Come back with {dbItem['price']*quantity - dbUser['balance']} more credits")
                 else:
@@ -46,8 +48,11 @@ class Market(commands.Cog):
                     userbridge.Update(dbReciever)
                     itembridge.Update(dbItem)
                     await ctx.send(f"Successful bought {dbItem['name']} from {dbItem['owner']}")
+        except AttributeError:
+            await ctx.send('This item does not exist')
         except:
             await ctx.send('There was an error')
+            traceback.print_exc()
 
     @commands.command()
     async def remove(self, ctx, id: int):
@@ -59,19 +64,19 @@ class Market(commands.Cog):
             await ctx.send(f'Your item {dbItem["name"]} has been removed from the market')
         except:
             await ctx.send('There was an error')
-
+            traceback.print_exc()
     @commands.command()
-    #returns the items the user has or queries for the items that contains the keyword
-    async def market(self, ctx, argument: String = None):
+    #returns the items the user is selling or queries for the items that contains the keyword
+    async def market(self, ctx, name = None):
         user = ctx.message.author
-        if(String):
+        if(name == None):
             try:
                 dbItem = itembridge.GetByUser(str(user))
                 embedString = ''
                 for item in dbItem:
                     embedString += f'iid: {item.iid} | Name: {item.name} | Quantity : {item.quantity} | Price: {item.price}\n'
                 message = discord.Embed(
-                    title = f"Items sold by {dbItem.owner}",
+                    title = f"Items sold by {user}",
                     description = embedString,
                     color=0xdd12f8,
                     thumbnail='https://mma.prnewswire.com/media/1096637/Royal_Canadian_Mint_The_Royal_Canadian_Mint_Launches_its_Largest.jpg?p=publish'
@@ -79,14 +84,16 @@ class Market(commands.Cog):
                 await ctx.send(embed=message)
             except:
                  await ctx.send('There was an error')
+                 traceback.print_exc()
         else:
             try:
-                dbItem = itembridge.GetByName(argument)
+                dbItem = itembridge.GetByItemName(name)
                 embedString = ''
                 for item in dbItem:
-                    embedString += f'iid: {item.iid} | Name: {item.name} | Quantity : {item.quantity} | Price: {item.price} | Owner: {item.owner}\n'
+                    item = item.to_dict()
+                    embedString += f"iid: {item['iid']} | Name: {item['name']} | Quantity : {item['quantity']} | Price: {item['price']} | Owner: {item['owner']}\n"
                 message = discord.Embed(
-                    title = f"Items sold with keyword: {argument}",
+                    title = f"Items sold with keyword: {name}",
                     description = embedString,
                     color=0xdd12f8,
                     thumbnail='https://mma.prnewswire.com/media/1096637/Royal_Canadian_Mint_The_Royal_Canadian_Mint_Launches_its_Largest.jpg?p=publish'
@@ -94,7 +101,8 @@ class Market(commands.Cog):
                 await ctx.send(embed=message)
             except:
                  await ctx.send('There was an error')
-            
+                 traceback.print_exc()
+
 def setup(bot):
     bot.add_cog(Market(bot))     
 
